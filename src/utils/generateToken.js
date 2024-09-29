63398f14a58eed12b22f12a50620e64e4f80d11a
Tohka <1324419148@qq.com>
2024-09-29 17:51:48 +0800

    feat: 添加生成token的工具函数generateToken

---
+++ src/utils/generateToken.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "Tohka";
+const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || "Tohka10xiang";
+const ACCESS_TOKEN_EXPIRES_IN = process.env.ACCESS_TOKEN_EXPIRES_IN || "15m";
+const REFRESH_TOKEN_EXPIRES_IN = process.env.REFRESH_TOKEN_EXPIRES_IN || "7d";
+
+// 生成访问令牌
+const generateAccessToken = (payload) => {
+    return jwt.sign(payload, ACCESS_TOKEN_SECRET, {
+        expiresIn: ACCESS_TOKEN_EXPIRES_IN,
+    });
+};
+
+// 生成刷新令牌
+const generateRefreshToken = (payload) => {
+    return jwt.sign(payload, REFRESH_TOKEN_SECRET, {
+        expiresIn: REFRESH_TOKEN_EXPIRES_IN,
+    });
+};
