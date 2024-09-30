ad9c6c73a884ead75f43d6587dfd9bf7682881da
Tohka <1324419148@qq.com>
2024-09-30 10:50:08 +0800

    feat: 更改refreshToken的过期时间为1d

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "Tohka";
 const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || "Tohka10xiang";
 const ACCESS_TOKEN_EXPIRES_IN = process.env.ACCESS_TOKEN_EXPIRES_IN || "15m";
-const REFRESH_TOKEN_EXPIRES_IN = process.env.REFRESH_TOKEN_EXPIRES_IN || "7d";
+const REFRESH_TOKEN_EXPIRES_IN = process.env.REFRESH_TOKEN_EXPIRES_IN || "1d";
 
 // 生成访问令牌
 export const generateAccessToken = (payload) => {
