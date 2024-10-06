0c3338c6f127d712f1b808f964d070761e95fbcc
Tohka <1324419148@qq.com>
2024-10-06 15:46:01 +0800

    feat: 引入nonce字段在生成token
    导入crypto库，确保nonce不同且唯一，确保生成token唯一

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,20 +1,30 @@
 import jwt from "jsonwebtoken";
 import "dotenv/config";
+import * as crypto from "crypto";
+
+//密钥
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "Tohka";
 const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || "Tohka10xiang";
+//过期时间
 const ACCESS_TOKEN_EXPIRES_IN = process.env.ACCESS_TOKEN_EXPIRES_IN || "15m";
 const REFRESH_TOKEN_EXPIRES_IN = process.env.REFRESH_TOKEN_EXPIRES_IN || "1d";
 
 // 生成访问令牌
 export const generateAccessToken = (payload) => {
-    return jwt.sign(payload, ACCESS_TOKEN_SECRET, {
+    const accessTokenNonce = crypto.randomUUID();
+    payload.nonce = accessTokenNonce;
+    const accessToken = jwt.sign(payload, ACCESS_TOKEN_SECRET, {
         expiresIn: ACCESS_TOKEN_EXPIRES_IN,
     });
+    return { accessToken, accessTokenNonce };
 };
 
 // 生成刷新令牌
 export const generateRefreshToken = (payload) => {
-    return jwt.sign(payload, REFRESH_TOKEN_SECRET, {
+    const refreshTokenNonce = crypto.randomUUID();
+    payload.nonce = refreshTokenNonce;
+    const refreshToken = jwt.sign(payload, REFRESH_TOKEN_SECRET, {
         expiresIn: REFRESH_TOKEN_EXPIRES_IN,
     });
+    return { refreshToken, refreshTokenNonce };
 };
