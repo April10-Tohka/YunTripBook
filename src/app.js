a842bd49baaf17990d74cf01c089cf803766c3c2
Tohka <1324419148@qq.com>
2024-10-06 17:26:31 +0800

    扩展测试路由,cookie的获取

--- src/app.js
+++ src/app.js
@@ -8,21 +8,28 @@ import {
     loggerMiddleWare,
     loggerErrorMiddleWare,
 } from "./middlewares/logger.js";
+
+// 引入路由中间件
+import { verifyAccessToken } from "./middlewares/verifyToken.js";
+
 //创建服务器
 const app = express();
+
 //全局中间件
 app.use(loggerMiddleWare);
 app.use(express.json());
+
 //使用路由
 app.use("/api", uploadRoute);
 app.use("/auth", authRoute);
 
 //测试路由
-app.get("/test/123", (req, res) => {
-    console.log("路由123");
-    setTimeout(() => {
-        res.json({ a: 1 });
-    }, 300);
+app.get("/test/verify-access-token", verifyAccessToken, (req, res) => {
+    console.log("校验access token 路由,中间件通过后就会看到这句话");
+    console.log("查看req.user:", req.user);
+    const refreshToken = req.headers.cookie.split("=")[1];
+    console.log("refreshToken:", refreshToken);
+    res.json(req.user);
 });
 
 //错误全局中间件
