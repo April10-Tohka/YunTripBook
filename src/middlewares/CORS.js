9a24bb4b371ec96ef84091b8e01fbe27c8f86193
Tohka <1324419148@qq.com>
2024-10-25 17:25:24 +0800

    feat: 添加响应头字段

--- src/middlewares/CORS.js
+++ src/middlewares/CORS.js
@@ -4,7 +4,8 @@ export const setResponseHeader = (req, res, next) => {
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS,PUT"); // 允许的请求方法
     res.setHeader("Access-Control-Allow-Headers", [
         "Content-Type",
-        "authorization",
-    ]); // 允许的请求头
+        "Authorization",
+        "RefreshToken",
+    ]);
     next();
 };
