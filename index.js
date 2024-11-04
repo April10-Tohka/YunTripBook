d1826ad2338deed3063042e8c1e8ef29318b5fe1
Tohka <1324419148@qq.com>
2024-11-04 11:43:32 +0800

    feat: 服务器启动前，初始化rabbitMQ

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import app from "./src/app.js";
 import config from "./src/config/index.js";
-
+import { initRabbitMQ } from "./src/utils/rabbitMQ.js";
 const PORT = config.server.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`服务器启动！地址为:  http://localhost:${PORT}`);
-});
+const startServer = () => {
+    initRabbitMQ().then(() => {
+        app.listen(PORT, () => {
+            console.log(`服务器启动！地址为:  http://localhost:${PORT}`);
+        });
+    });
+};
+
+startServer();
