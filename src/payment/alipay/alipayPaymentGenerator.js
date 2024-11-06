50bbf16f57397403c526c6f51523a985abffa95e
Tohka <1324419148@qq.com>
2024-11-06 11:59:38 +0800

    feat: Function-添加生成支付宝的支付订单函数

--- src/payment/alipay/alipayPaymentGenerator.js
+++ src/payment/alipay/alipayPaymentGenerator.js
@@ -1,5 +1,18 @@
 import alipaySdk from "./alipayConfig.js";
 
-const generateAlipayPaymentOrder = (orderInfo) => {
-    //生成支付订单
+/**
+ * 生成支付宝的支付订单,返回支付链接
+ * @param orderInfo 订单信息(订单号，金额，订单标题)
+ * @returns {string} 支付链接url
+ */
+export const generateAlipayPaymentOrder = (orderInfo) => {
+    const paymentParams = {
+        bizContent: {
+            out_trade_no: orderInfo.out_trade_no,
+            total_amount: orderInfo.total_amount,
+            subject: orderInfo.subject,
+            product_code: "FAST_INSTANT_TRADE_PAY",
+        },
+    };
+    return alipaySdk.pageExecute("alipay.trade.page.pay", "GET", paymentParams);
 };
