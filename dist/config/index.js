290a9d1d6213b2d8faec1e2159598c1ec3344cb2
Tohka <1324419148@qq.com>
2024-11-07 17:14:42 +0800

    feat: 删除dist文件夹

--- dist/config/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    server: {
-        PORT: process.env.SERVER_PORT,
-    },
-};
-exports.default = config;
