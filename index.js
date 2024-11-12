1ecc891c077f5e1e463494764898da9e9ddf0043
Tohka <1324419148@qq.com>
2024-11-12 16:23:48 +0800

    feat: 更改服务器启动的log语句，显示服务器地址

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import app from "./src/app.js";
 import config from "./src/config/index.js";
 import { initRabbitMQ } from "./src/utils/rabbitMQ.js";
-const PORT = config.server.PORT || 3000;
+const PORT = config.server.PORT;
 
 const startServer = () => {
     initRabbitMQ().then(() => {
         app.listen(PORT, () => {
-            console.log(`服务器启动！地址为:  http://localhost:${PORT}`);
+            console.log(`服务器启动！地址为:  http://8.138.9.94:${PORT}`);
         });
     });
 };
