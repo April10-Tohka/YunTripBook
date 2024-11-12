3653c1c35134e6d96333614f4551735c94330d06
Tohka <1324419148@qq.com>
2024-11-12 17:02:10 +0800

    feat(origin): 环境不同，响应头允许的源地址不同

--- src/middlewares/CORS.js
+++ src/middlewares/CORS.js
@@ -1,4 +1,8 @@
-const origin = "http://localhost:5173";
+import config from "../config/index.js";
+const origin =
+    config.server.NODE_ENV === "development"
+        ? "http://localhost:5173"
+        : "http://8.138.9.94";
 export const setResponseHeader = (req, res, next) => {
     res.header("Access-Control-Allow-Origin", origin);
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS,PUT"); // 允许的请求方法
